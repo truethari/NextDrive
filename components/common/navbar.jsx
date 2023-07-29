3040ca6f99bc534608b3c2f7b43a1b3d3ab435b6
Tharindu N <40334396+truethari@users.noreply.github.com>
2023-07-29 16:38:57 +0530

    version 1.1

--- components/common/navbar.jsx
+++ components/common/navbar.jsx
@@ -3,7 +3,7 @@
 import React from "react";
 import Link from "next/link";
 
-import { Moon, Sun } from "lucide-react";
+import { Moon, Sun, Github } from "lucide-react";
 import { useTheme } from "next-themes";
 
 import { Button } from "@/components/ui/button";
@@ -21,8 +21,8 @@ export function ModeToggle() {
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
         <Button variant="ghost" size="icon">
-          <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
-          <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+          <Sun className="h-[1rem] w-[1rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
+          <Moon className="absolute h-[1rem] w-[1rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
           <span className="sr-only">Toggle theme</span>
         </Button>
       </DropdownMenuTrigger>
@@ -37,7 +37,7 @@ export function ModeToggle() {
 
 export default function NavBar() {
   return (
-    <nav className="bg-white shadow dark:bg-zinc-900 fixed top-0 left-0 w-full h-16 z-50">
+    <nav className="bg-white shadow dark:bg-zinc-900 fixed top-0 left-0 w-full h-14 z-50">
       <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-2">
         <Link href="/" className="flex items-center">
           <span className="self-center text-xl font-semibold whitespace-nowrap dark:text-white">
@@ -48,6 +48,16 @@ export default function NavBar() {
         <div className="flex items-center md:order-2">
           <div className="mr-2">
             <ModeToggle />
+          </div>
+          <div className="cursor-pointer">
+            <a
+              href="https://github.com/truethari/NextDrive"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <Github className="h-[1rem] w-[1rem]" />
+              <span className="sr-only">GitHub</span>
+            </a>
           </div>
         </div>
       </div>
